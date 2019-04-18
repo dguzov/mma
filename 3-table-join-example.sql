8c6e5cb94b1a817472595efa34558e775fc93722
dguzov <anotherdayofsun777@gmail.com>
2019-04-17 23:58:54 -0400

    v.1.1

--- 3-table-join-example.sql
+++ 3-table-join-example.sql
@@ -1,6 +1,7 @@
-SELECT p.Code, p.Description, i.OrderNumber, i.CustomerName, l.Quantity, i.Total
+SELECT p.Code, p.Description as 'Item Description', i.OrderNumber as 'Order Number', 
+i.CustomerName as 'Customer Name', l.Quantity, i.Total as '$, Total'
 from product p
 	join lineitem l
 		on p.id = l.productid
 	join invoice i
-		on l.invoiceid = i.id
+		on l.invoiceid = i.id;
